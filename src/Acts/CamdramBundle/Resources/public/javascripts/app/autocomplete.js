77bfd7bc5bac231ee62d9aa1a3a0d01fdf34469f
Peter Birch <peter@lightlogic.co.uk>
2013-08-21 16:02:23 +0100

    Starting to make the changes Peter H suggested on ticket 43

--- src/Acts/CamdramBundle/Resources/public/javascripts/app/autocomplete.js
+++ src/Acts/CamdramBundle/Resources/public/javascripts/app/autocomplete.js
@@ -7,7 +7,7 @@ Camdram.autocomplete.items = [];
 $(function() {
     $(".autocomplete .searchfield").focus(function(e) {
         $(this).closest(".autocomplete").addClass("active");
-        Camdram.autocomplete.suggest(this);
+        Camdram.autocomplete.trigger(this);
     }).blur(function(e) {
         $(this).closest(".autocomplete").removeClass("active");
         Camdram.autocomplete.drawControl(false);
@@ -16,8 +16,11 @@ $(function() {
             Camdram.autocomplete.shiftOption(this, (e.keyCode == 40));
         else if(e.keyCode == 13)
             Camdram.autocomplete.chooseOption(this);
-        else if(e.keyCode != 37 && e.keyCode != 39)
-            Camdram.autocomplete.suggest(this);
+        else if(e.keyCode == 27) {
+            $(this).val('');
+            Camdram.autocomplete.drawControl(false);
+        } else if(e.keyCode != 37 && e.keyCode != 39)
+            Camdram.autocomplete.trigger(this);
         e.preventDefault();
         return false;
     });
@@ -26,6 +29,13 @@ $(function() {
         return false;
     });
 });
+
+// We don't want to continuously trigger as they type, so timeout
+Camdram.autocomplete.timeout = null;
+Camdram.autocomplete.trigger = function(field) {
+    if(Camdram.autocomplete.timeout != null) clearTimeout(Camdram.autocomplete.timeout);
+    Camdram.autocomplete.timeout = setTimeout(function() { Camdram.autocomplete.suggest(field) }, 250);
+};
 
 Camdram.autocomplete.chooseOption = function(field) {
     // Find if we currently have a selected item
@@ -77,8 +87,14 @@ Camdram.autocomplete.shiftOption = function(field, down) {
 
 Camdram.autocomplete.drawControl = function(show, height) {
     if (height == null) height = 40;
-    if(show) $(".autocomplete .results").css("display", "block").animate({"height": height + "px", "opacity": 1.0}, 250);
-    else $(".autocomplete .results").animate({"height": "0px", "opacity": 0.0, "display": "none"}, 250);
+    if(show) {
+        $(".autocomplete").animate({"height": height + 37 + "px"}, 250);
+        $(".autocomplete .results").css({"display": "block", "height": height + "px"}).animate({"opacity": 1.0}, 250);
+    }
+    else {
+        $(".autocomplete").animate({"height": "40px"}, 250);
+        $(".autocomplete .results").animate({"opacity": 0.0, "display": "none"}, 250);
+    }
 }
 
 Camdram.autocomplete.suggest = function(field) {
@@ -99,36 +115,56 @@ Camdram.autocomplete.suggest = function(field) {
             $(".noresults").css("display", "none");
             for (var i = 0; i < Camdram.autocomplete.items.length; i++) {
                 var result = Camdram.autocomplete.items[i];
-                var item = document.createElement("li");
-                item.onclick = (function(data_item) { return function() {
-                    window.location.href = Routing.generate('get_entity', {id: data_item.id});
-                };})(result);
-                // Add in the icon
-                var icon = document.createElement("i");
-                icon.className = "icon-user";
-                if(result.camdram_type == "show") icon.className = "icon-ticket";
-                else if(result.camdram_type == "venue") icon.className = "icon-building";
-                item.appendChild(icon);
-                // Add in the text
-                var text = document.createElement("span");
-                text.className = "resultText";
-                text.innerHTML = result.name;
-                item.appendChild(text);
+                var item = Camdram.autocomplete._generate_item(result);
                 // Add item into the page
                 $(".autocomplete .results ul").append(item);
-                // Autosize text
-                $(text).quickfit({
-                    "min": 12,
-                    "max": 16,
-                    "truncate": true,
-                    "width": 440
-                });
             }
-            Camdram.autocomplete.drawControl(true, (data.length * 40));
+            Camdram.autocomplete.drawControl(true, (data.length * 39));
         } else {
             $(".noresults").css("display", "block");
             Camdram.autocomplete.drawControl(true);
         }
         $(".autocomplete .status-icon").removeClass("icon-spinner").removeClass("icon-spin").addClass("icon-search");
     });
+}
+
+Camdram.autocomplete._generate_item = function(result) {
+    var item = document.createElement("li");
+    item.onclick = (function(data_item) { return function() {
+        window.location.href = Routing.generate('get_entity', {id: data_item.id});
+    };})(result);
+    // Add in the icon
+    var icon = document.createElement("i");
+    icon.className = "icon-user";
+    if(result.camdram_type == "show") icon.className = "icon-ticket";
+    else if(result.camdram_type == "venue") icon.className = "icon-building";
+    else if(result.camdram_type == "society") icon.className = "icon-group";
+    item.appendChild(icon);
+    // Add in the text segments
+    var textholder = document.createElement("div");
+    textholder.className = "textHolder";
+    item.appendChild(textholder);
+    // Add in the text
+    var text = document.createElement("div");
+    text.className = "resultText";
+    text.innerHTML = result.name;
+    textholder.appendChild(text);
+    if(result.camdram_type == "show") {
+        // Wind up text
+        text.style.marginTop = "-5px";
+        // Add in subtext
+        var subtext = document.createElement("div");
+        subtext.className = "subText";
+        subtext.innerHTML = result.dates + " - " + result.venue_name;
+        textholder.appendChild(subtext);
+    }
+    // Autosize text
+    $(text).quickfit({
+        "min": 12,
+        "max": 16,
+        "truncate": true,
+        "width": 500
+    });
+    
+    return item;
 }
